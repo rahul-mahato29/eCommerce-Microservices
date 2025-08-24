d03159440d1564aebf1548a90bdfaaedc3f58a78
Rahul Mahato <mahatorahul2901@gmail.com>
2025-08-25 01:49:52 +0530

    Added API-Gateway Route Specific Filter : OrderService Specific

--- API-Gateway/src/main/java/com/microservices/API_Gateway/filters/LoggingOrdersFilter.java
+++ API-Gateway/src/main/java/com/microservices/API_Gateway/filters/LoggingOrdersFilter.java
@@ -1,4 +1,27 @@
 package com.microservices.API_Gateway.filters;
 
-public class LoggingOrdersFilter {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LoggingOrdersFilter extends AbstractGatewayFilterFactory<LoggingOrdersFilter.config> {
+
+    public LoggingOrdersFilter() {
+        super(config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(config config) {
+        return (exchange, chain) -> {
+            log.info("Order Filter Pre : {}", exchange.getRequest().getURI());
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class config {
+
+    }
 }
